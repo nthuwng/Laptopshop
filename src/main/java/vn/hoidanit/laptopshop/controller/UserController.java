5e4a5d09b37cfce4edd0dbabe4ad386b53db18dd
Git cua Hung <nguyenthinhhung150603@gmail.com>
2024-11-13 22:54:23 +0700

    bai58

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -34,7 +34,7 @@ public class UserController {
 
     @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
     public String createUserPage(Model model, @ModelAttribute("newUser") User hoidanit) {
-        System.out.println("run here"+hoidanit);
+        System.out.println("run here "+hoidanit);
         return "hello";
     }
 }
