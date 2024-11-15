2af9bbd3822beb6ded3cfc301e252809c553b721
Git cua Hung <nguyenthinhhung150603@gmail.com>
2024-11-15 21:35:52 +0700

    bai60

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import vn.hoidanit.laptopshop.domain.User;
+import vn.hoidanit.laptopshop.repository.UserRepository;
 import vn.hoidanit.laptopshop.service.UserService;
 
 @Controller
@@ -20,8 +21,7 @@ public class UserController {
 
     @RequestMapping("/")
     public String getHomePage(Model model) {
-        String test = this.userService.handleHello();
-        model.addAttribute("eric", test);
+        model.addAttribute("eric", "test");
         model.addAttribute("hoidanit", "from controller with model");
         return "hello";
     }
@@ -33,8 +33,9 @@ public class UserController {
     }
 
     @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
-    public String createUserPage(Model model, @ModelAttribute("newUser") User hoidanit) {
+    public String createUserPage(Model model, @ModelAttribute("newUser") User hoidanit) { //@ModelAttribute dùng để lấy dữ liệu từ form vào object
         System.out.println("run here "+hoidanit);
+        this.userService.handleSaveUser(hoidanit);
         return "hello";
     }
 }
--- src/main/java/vn/hoidanit/laptopshop/domain/User.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/User.java
@@ -7,7 +7,7 @@ import jakarta.persistence.Id;
 
 @Entity
 public class User {
-    @Id 
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String email;
