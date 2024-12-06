06b6e77a98467ee21eef0f2cf795ce428d044e16
Git cua Hung <nguyenthinhhung150603@gmail.com>
2024-12-06 11:12:29 +0700

    n - n

--- src/main/java/vn/hoidanit/laptopshop/domain/Order.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/Order.java
@@ -1,11 +1,14 @@
 package vn.hoidanit.laptopshop.domain;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -16,9 +19,13 @@ public class Order {
     private long id;
     private double totalPrice;
 
+    // Nó là owner của quan hệ nên phải định nghĩa joinColumn
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
+
+    // @OneToMany(mappedBy = "order")
+    // List<OrderDetail> orderDetails;
 
     public long getId() {
         return id;
